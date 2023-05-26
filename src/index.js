df3fdb8b21fe06f02a18dc53320de7a8439f7413
Hello0801World <shun19036@yahoo.co.jp>
2023-05-25 21:35:10 -0500

    added unnecessary select and css lol

--- src/index.js
+++ src/index.js
@@ -22,27 +22,52 @@ const secondBook = {
 function BookList(){
     return (
         <section className='booklist'>
-            <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} /> { /* pass the object name and value */ }
-            <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+            <Book 
+            img={firstBook.img} 
+            title={firstBook.title} 
+            author={firstBook.author} 
+            > 
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing 
+                    and typesetting industry. Lorem Ipsum has been 
+                    the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled 
+                    it to make a type specimen book. It has survived not only 
+                    five centuries, but also the leap into electronic typesetting, 
+                    remaining essentially unchanged. It was popularised in the 1960s 
+                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+                <select name="cars" id="cars">
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                </select>
+            </Book> { /* pass the object name and value */ }
+
+            <Book img={secondBook.img} 
+            title={secondBook.title} 
+            author={secondBook.author} />
         </section>            
     );
 }
 
-// porps (properties)
+// props (properties)
 const Book = (props) => {
     console.log(props.title)
     // const title2 = props.job
     // variable
     
+    const { img, title, author} = props  // destructure object
 
     return (
         <article className='book'>
-            <img className='bookImg' src={props.img} alt="" />
-            <h1>{props.title}</h1>
+            <img className='bookImg' src={img} alt="" />
+            <h1>{title}</h1>
             <h4 style={{color: '#617d98', fontSize: '0.75rem', 
             marginTop: '0.25' }}>
-            {props.author}
+            {author}
             </h4>
+            {props.children}
             <div>
                 <h2>Comment</h2>
                 <input type="text" />
